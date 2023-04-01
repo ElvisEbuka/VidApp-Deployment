725afcf01cbfd7c56a3ae0b0b6cad2c7e9cc29cc
elvis <clecelvise@gmail.com>
2023-04-01 13:11:22 +0100

    updated the vid app

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 require('dotenv').config();
-const connectDB = require('./src/db/connect')
+const connectDB = require('./db/connect')
 mongoose.set('debug', true);
-const customerRouter = require('./src/routes/customers');
-const genreRouter = require('./src/routes/genres');
-const movieRouter = require('./src/routes/movies');
-const authRouter = require('./src/routes/auth');
-const rentalRouter = require('./src/routes/rentals');
+const customerRouter = require('./routes/customers');
+const genreRouter = require('./routes/genres');
+const movieRouter = require('./routes/movies');
+const authRouter = require('./routes/auth');
+const rentalRouter = require('./routes/rentals');
 
 
 //initializing the server
